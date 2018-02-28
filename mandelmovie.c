59b029be2a389e20d73c5eb249141f471b79bb07
Gabriel King <gking5@nd.edu>
2018-02-28 16:26:11 -0500

    sdlk

--- mandelmovie.c
+++ mandelmovie.c
@@ -7,6 +7,7 @@
 #include <sys/wait.h>
 //#include <sys/types.h>
 #include <unistd.h>
+#include <string.h>
 
 #define MAXSVAL 2
 #define MINSVAL .00001
@@ -14,7 +15,7 @@
 int startedImages = 0; 
 float sIncrement = (MAXSVAL - MINSVAL) / 50;
 float sVal = 2;
-char *mandelCommand[] = {"mandel", "-W 500 -H 500 -x 0.35 -y 0.1 -m 2000 -s ", "000000000", NULL};// TODO add variable s value to this command!!!
+char *mandelCommand[] = {"mandel", "-W",  "500",  "-H",  "500", "-x", "0.35", "-y",  "0.1", "-m", "2000", "-s", "000000000", "-o", "mandel00.bmp", NULL};// TODO add variable s value to this command!!!
 
 void usage() {
   printf("usage: ./mandelmovie \t<MAXPROCESSES>\n");
@@ -34,6 +35,14 @@ int forkMandel() {
     }
   } else {
     sVal -= sIncrement;
+    char bmpName[12];
+    strcpy(bmpName, "mandel");
+    char numbers[2] = "\0\0"; 
+    sprintf(numbers, "%02d", startedImages);
+    strcat(bmpName, numbers);
+    strcat(bmpName, ".bmp");
+    printf(bmpName);
+    //sprintf(mandelCommand[14], "mandel%d")
     //printf("%.2f", sVal); // TODO weird seg fault from next line
     //sprintf(mandelCommand[2], "test");
     startedImages++;
@@ -55,6 +64,8 @@ int main(int argc, char *argv[]) {
   if (maxProcesses < 1) { // validate max processes
     printf("error: MAXPROCESSES must be greater than 0\n");
     return -1;
+  } else if (maxProcesses > 50) {
+    
   }
 
   //for (int i = 0; i < 50; i++) {
